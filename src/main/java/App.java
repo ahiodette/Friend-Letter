129cea66a01a9b3db39062bc621c5f80210537f7
ahiodette <ahiode6@gmail.com>
2019-09-12 21:48:57 +0200

    Submit form

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -29,5 +29,14 @@ public class App {
             return new ModelAndView(model, "form.hbs");
         }, new HandlebarsTemplateEngine());
 
+        get("/greeting_card", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            String recipient = request.queryParams("recipient");
+            String sender = request.queryParams("sender");
+            model.put("recipient", recipient);
+            model.put("sender", sender);
+            return new ModelAndView(model, "greeting_card.hbs");
+        }, new HandlebarsTemplateEngine());
+
     }
 }
